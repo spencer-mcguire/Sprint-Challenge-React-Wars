baf95c8b01081f3f8451460c7129f5f3514c19b4
spencer-mcguire <smcguire1826@gmail.com>
2019-11-08 11:25:25 -0600

    complete MVP

--- starwars/src/components/Body.js
+++ starwars/src/components/Body.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "reactstrap";
+import axios from "axios";
+import { Container, Row } from "reactstrap";
 import Card from "./Card";
 
 const Body = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("https://lambda-swapi.herokuapp.com/api/people/")
+			.then(res => setData(res.data.results))
+			.catch(err => console.log(`HOLD ON: `, err));
+	}, []);
 	return (
 		<Container>
-			<Card />
+			<Row>
+				{data.map((a, index) => {
+					return (
+						<Card
+							key={index}
+							name={a.name}
+							gender={a.gender}
+							birth_year={a.birth_year}
+						/>
+					);
+				})}
+			</Row>
 		</Container>
 	);
 };
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,23 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import { Col } from "reactstrap";
 
 const CardContainer = styled.div`
-text-align: center
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-align-items: space-around;
-width: 300px;
-height: 300px;
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: space-around;
+	width: 250px;
+	height: 250px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
-const Card = () => {
+const Card = ({ name, gender, birth_year }) => {
 	return (
-		<CardContainer>
-			<h2>Name</h2>
-			<p>Gender</p>
-			<p>BirthDate</p>
-		</CardContainer>
+		<Col xs='12' s='6' md='4'>
+			<CardContainer>
+				<h2>{name}</h2>
+				<p>Gender: {gender}</p>
+				<p>Birth Date: {birth_year}</p>
+			</CardContainer>
+		</Col>
 	);
 };
 export default Card;
